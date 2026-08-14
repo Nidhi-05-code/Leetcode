b2085779eea1619cb689e7db270be5e80ae0da8a
Nidhi-05-code <nidhichabbi06@gmail.com>
2026-08-14 12:29:38 +0530

    Time: 102 ms (61.16%), Space: 51.1 MB (54.36%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int countPrimes(int n) {
+
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+
+        // Assume every number is prime initially
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Cross out multiples
+        for (int i = 2; i * i < n; i++) {
+
+            if (isPrime[i]) {
+
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Count remaining primes
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
